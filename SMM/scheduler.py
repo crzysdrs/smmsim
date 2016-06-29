48ec546c03ad4e8ab95f6aeea7b20d8c9adbcdf4
Mitch Souders <mitch.souders@gmail.com>
2016-06-28 20:47:23 -0700

    Initial Commit

---
+++ SMM/scheduler.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+class Check:
+    def __init__(self):
+        pass
+
+class Task:
+    def __init__(self):
+        pass
+
+class Bin:
+    def __init__(self):
+        pass
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+Setup.py
+"""
+from setuptools import setup
+
+setup(
+    name='SMM',
+    version='0.1',
+    description='SMM Scheduler Simulator',
+    author='Mitch Souders',
+    author_email='msouders@pdx.edu',
+    scripts=[
+    ],
+    #package_dir={'': 'SMM'},
+    #py_modules=[
+    #    'SMM'
+    #],
+    entry_points={
+        'console_scripts': [
+            'smm_sim = SMM.simulator:main'
+        ],
+    },
+    install_requires=[
+    ]
+)
