c7f49cb087071dced0390ca77fb05cdd394c8ae8
Mitch Souders <mitch.souders@gmail.com>
2016-06-29 20:16:17 -0700

    Add Checks and Task Splitting

--- SMM/scheduler.py
+++ SMM/scheduler.py
@@ -1,12 +1,63 @@
 #!/usr/bin/env python
 
+class CheckGroup:
+    def __init__(self, name, subchecks=None):
+        self.__name = name
+        if not subchecks:
+            subchecks = []
+
+        self.__subchecks = []
+
+        for sc in subchecks:
+            self.addSubCheck(sc)
+
+    def addSubCheck(self, sc):
+        sc.setGroup(self)
+        self.__subchecks.append(sc)
+
+    def getChecks(self):
+        return self.__subchecks
+
+    def getGroupCost(self):
+        return sum([sc.getCost() for sc in self.__subchecks])
+
+    def getName(self):
+        return self.__name
+
 class Check:
-    def __init__(self):
-        pass
+    def __init__(self, name, cost):
+        self.__name = name
+        self.__cost = cost
+        self.__group = None
+
+    def setGroup(self, group):
+        self.__group = group
+
+    def makeTask(self, index, cost):
+        return Task(self, index, cost)
+
+    def getCost(self):
+        return self.__cost
+
+    def __str__(self):
+        if self.__group:
+            parent = self.__group.getName()
+        else:
+            parent = '<UNDEFINED>'
+
+        return "{}.{}".format(parent, self.__name)
 
 class Task:
-    def __init__(self):
-        pass
+    def __init__(self, subcheck, index, cost):
+        self.__subcheck = subcheck
+        self.__index = index
+        self.__cost = cost
+
+    def getCost(self):
+        return self.__cost
+
+    def __str__(self):
+        return "{}.{}".format(self.__subcheck, self.__index)
 
 class Bin:
     def __init__(self):
--- SMM/simulator.py
+++ SMM/simulator.py
@@ -1,8 +1,68 @@
 #!/usr/bin/env python
 
-import scheduler
+from scheduler import CheckGroup, Check, Task
+
+def tasksplitter(checkgroups):
+    tasksize = 50
+    tasks = []
+    for cg in checkgroups:
+        for c in cg.getChecks():
+            cost = c.getCost()
+            i = 0
+            while cost > 0:
+                task_cost = tasksize if cost > tasksize else cost
+                tasks.append(c.makeTask(i, task_cost))
+                cost -= tasksize
+                i += 1
+
+    return tasks
+
 def main():
-    print "Hello World"
+    checks = [
+        CheckGroup(
+            'IDTcheck', [
+                Check('IDTR', 1),
+                Check('HashIdtTable', 10),
+                Check('IdtFunctionPtrWalker', 15),
+            ]
+        ),
+        CheckGroup(
+            'HyperCall', [
+                Check('HypercallTableAddr', 5),
+                Check('HypercallTableHash', 50),
+                Check('HypercallTableWalk', 100),
+            ]
+        ),
+        CheckGroup(
+            'GDT', [
+                Check('GDTR', 1),
+                Check('GdtHash', 50),
+            ]
+        ),
+        CheckGroup(
+            'Kernel', [
+                Check('KernelAddr', 20),
+                Check('KernelHash', 500),
+            ]
+        ),
+        CheckGroup(
+            'VmExit', [
+                Check('VmExitTableAddr', 1),
+                Check('VmExitTableHash', 5),
+                Check('VmExitTableWalk', 10),
+            ]
+        ),
+        CheckGroup(
+            'CR0', [
+                Check('CR0Val', 1),
+            ]
+        ),
+    ]
+
+    tasks = tasksplitter(checks)
+
+    for t in tasks:
+        print t
 
 if __name__ == "__main__":
     main()
