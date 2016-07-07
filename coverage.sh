d490e80be93e3586e0caca620099ffd4ef089ec2
Mitch Souders <mitch.souders@gmail.com>
2016-07-06 23:00:34 -0700

    Add coverage run commands

--- coverage.sh
+++ coverage.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
-smm_sim 10 || exit 1
+coverage run smm_sim 10 || exit 1
 
-smm_sim 10 --task_granularity 20 --bin_size 60 --smm_overhead 30 || exit 1
+coverage run smm_sim 10 --task_granularity 20 --bin_size 60 --smm_overhead 30 || exit 1
 
-smm_sim 10 --sqllog test.db || exit 1
+coverage run smm_sim 10 --sqllog test.db || exit 1
 
-smm_sim 10 --binpacker MaxFillBin || exit 1
+coverage run smm_sim 10 --binpacker MaxFillBin || exit 1
 
-smm_sim 10 --binpacker MaxPriorityBin || exit 1
+coverage run smm_sim 10 --binpacker MaxPriorityBin || exit 1
 
-smm_sim 10 --binpacker RandomBin || exit 1
+coverage run smm_sim 10 --binpacker RandomBin || exit 1
