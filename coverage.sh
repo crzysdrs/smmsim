0d34ad156fdb97c3b2bbd3ef95b6bcb01cd033a8
Mitch Souders <mitch.souders@gmail.com>
2016-07-06 23:30:42 -0700

    Update coverage

--- coverage.sh
+++ coverage.sh
@@ -1,13 +1,19 @@
 #/bin/bash
 
-coverage run smm_sim 10 || exit 1
+SMM_SIM=`which smm_sim`
+COV_RUN="coverage run"
 
-coverage run smm_sim 10 --task_granularity 20 --bin_size 60 --smm_overhead 30 || exit 1
+$COV_RUN $SMM_SIM 10 || exit 1
 
-coverage run smm_sim 10 --sqllog test.db || exit 1
+$COV_RUN $SMM_SIM 10 --task_granularity 20 --bin_size 60 --smm_overhead 30 || exit 1
 
-coverage run smm_sim 10 --binpacker MaxFillBin || exit 1
+$COV_RUN $SMM_SIM 10 --sqllog test.db || exit 1
 
-coverage run smm_sim 10 --binpacker MaxPriorityBin || exit 1
+$COV_RUN $SMM_SIM 10 --binpacker MaxFillBin || exit 1
 
-coverage run smm_sim 10 --binpacker RandomBin || exit 1
+$COV_RUN $SMM_SIM 10 --binpacker MaxPriorityBin || exit 1
+
+$COV_RUN $SMM_SIM 10 --binpacker RandomBin || exit 1
+
+coverage combine
+coverage report
