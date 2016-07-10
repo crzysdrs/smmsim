7b9a2a9576d9e0b0d9ea8a7ef36865ea0161fdd8
Mitch Souders <mitch.souders@gmail.com>
2016-07-10 15:51:38 -0700

    Add new benchmark for bins

--- SMM/benchmarks.py
+++ SMM/benchmarks.py
@@ -8,8 +8,6 @@ def timediff(conn):
     #Computes the minimum, maximum and mean of each tasks runtime seperation.
     #Effectively means that an ephemeral attack that is shorter than the min
     # that occurs outside of the detection period will never be detected.
-
-
     results = []
 
     for task in conn.cursor().execute("SELECT * FROM task order by id;"):
@@ -38,8 +36,9 @@ def cputime(conn):
 
 def bincount(conn):
     c = conn.cursor()
-    r = c.execute("select count(bin_id) as bin_count from (select bin_id from event where bin_id not null group by bin_id)").fetchall()
-    return r[0]['bin_count']
+    r = c.execute("select sum(length) as bin_length from event where bin_id not null group by bin_id").fetchall()
+    bindata = np.array(r)
+    return (len(bindata), np.min(bindata), np.max(bindata), np.mean(bindata) )
 
 def miscdata(conn):
     c = conn.cursor()
@@ -58,13 +57,13 @@ def main():
     conn.row_factory = sqlite3.Row
 
     print ("\n".join(map(lambda x : "Misc Data {:>15}:{}".format(x['key'], x['val']), miscdata(conn))))
-    print ("Total Bins Run: {}".format(bincount(conn)))
+    print ("Bins Total: {} \nBin Length min/max/avg (microseconds): {:>15} {:>15} {:>15.2f}".format(*bincount(conn)))
     #if we had any firm failure cases, we could easily check that the
     # min/max/average met certain criteria.
     print ("Time Betweeen Subsequent Repeated Task: count/min/max/avg (microseconds)")
     times = timediff(conn)
     for t in times:
-        print(("{:>50} {:>10}" + "{:>15.2f}" * 3).format(*t))
+        print(("{:>50} {:>10}" + "{:>15}" * 2 + "{:>15.2f}").format(*t))
 
     print ("SMM CPU Time Percentage: {}%".format(cputime(conn) * 100))
 
