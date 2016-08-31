18341c126975b955dc4df64d1042e7365473db8f
Mitch Souders <mitch.souders@gmail.com>
2016-08-30 23:26:42 -0700

    Changes to report info generating script

--- test/test.py
+++ test/test.py
@@ -55,6 +55,7 @@ def collect_result(b, fname):
     return (b, json_result)
 
 def main():
+    runsim = True
     binpackers = scheduler.getBinPackers()
     sim_time = 1000
 
@@ -64,49 +65,51 @@ def main():
     if not os.path.exists("tables"):
         os.mkdir("tables")
 
-    sim = [50, 60, 70, 80, 90, 95, 100]
+    sim = [70, 80, 90, 95]
     for s in sim:
         if not os.path.exists("results_{}".format(s)):
             os.mkdir("results_{}".format(s))
-        sp.call(
-            [
-                "smmrandwork",
-                str(sim_time),
-                str(s / 100),
-                "results_{sim}/sim.workload".format(sim=s),
-                "--cost-mu", "25",
-                "--cost-sigma", "50",
-                "--priority-mu", "10",
-                "--priority-sigma", "10",
-                "--skip-prelude"
-            ]
-        )
-        for b in binpackers.keys():
+        if runsim:
             sp.call(
                 [
                     "smmrandwork",
-                    "--prelude-only",
-                    "--binpacker={bp}".format(bp=b),
                     str(sim_time),
-                    str(1),
-                    "results_{sim}/{bp}.prelude".format(bp=b, sim=s)
+                    "results_{sim}/sim.workload".format(sim=s),
+                    "--load={}".format(s / 100),
+                    "--cost-mu", "25",
+                    "--cost-sigma", "50",
+                    "--priority-mu", "10",
+                    "--priority-sigma", "10",
+                    "--skip-prelude"
                 ]
             )
+            for b in binpackers.keys():
+                sp.call(
+                    [
+                        "smmrandwork",
+                        "--prelude-only",
+                        "--binpacker={bp}".format(bp=b),
+                        "--load={}".format(1),
+                        str(sim_time),
+                        "results_{sim}/{bp}.prelude".format(bp=b, sim=s)
+                    ]
+                )
 
 
     benchmarks = {}
-    short_benchmarks = {}
 
     p = Pool(5)
-    p.starmap(run_sim, [(b, s) for b in binpackers.keys() for s in sim])
+    if runsim:
+        p.starmap(run_sim, [(b, s) for b in binpackers.keys() for s in sim])
 
-    results = p.starmap(collect_result, [(b, "results_90/{bp}.log".format(bp=b)) for b in binpackers.keys()])
-    short_results = p.starmap(collect_result, [(b, "results_95/{bp}.log".format(bp=b)) for b in binpackers.keys()])
+    results = {}
+    for s in sim:
+        results[s] = p.starmap(collect_result, [(b, "results_{s}/{bp}.log".format(bp=b, s=s)) for b in binpackers.keys()])
 
-    for b,v in results:
-        benchmarks[b] = json.loads(v.decode())
-    for b,v in short_results:
-        short_benchmarks[b] = json.loads(v.decode())
+    for s in sim:
+        benchmarks[s] = {}
+        for b,v in results[s]:
+            benchmarks[s][b] = json.loads(v.decode())
 
     priorities = {}
     costs = {}
@@ -116,17 +119,44 @@ def main():
     }
     throughput_head = [(1, ""), (2, "Tasks/Bin")]
     throughput = {
-        '': ['90% Load', '95% Load']
+        '': ['70% Load', '95% Load']
     }
-    response_stat_head = [(1, ""), (2, "Reponse Cost (us)")]
+    response_stat_head = [(1, ""), (2, "Response Cost (us)")]
     response_stat = {
         '': ['Avg', 'Std Dev']
     }
-    runtime_head = [(1, ""), (2, "90\\% Load"), (2, "95\\% Load")]
+    runtime_head = [(1, ""), (2, "70\\% Load (s)"), (2, "95\\% Load (s)")]
     runtime = {
-        '': ['CPU Time', 'Wall Time', 'CPU Time', 'Wall Time']
+        '': ['CPU', 'Wall', 'CPU', 'Wall']
     }
-    for k,v in benchmarks.items():
+
+    sim_bin_data_head = [(1, "")] + [(2, "{}\\% Load".format(s)) for s in sim]
+    sim_bin_data = {
+        'workload': [s for s in sim]
+    }
+
+    sim_bin_resp_head = [(1, "")] + [(2, "{}\\% Load".format(s)) for s in sim]
+    sim_bin_resp = {
+        'workload': [s for s in sim]
+    }
+
+    for k,v in benchmarks[s].items():
+        sim_bin_data[k] = []
+        sim_bin_resp[k] = []
+
+    for s in sim:
+        for k,v in benchmarks[s].items():
+            sim_bin_data[k] += [v['bins']['length']['mean']]
+            sim_bin_resp[k] += [v['response_times']['mean']]
+
+    for k,v in benchmarks[70].items():
+        throughput[k] = [v['throughput_tasks_per_bin']]
+        runtime[k] = [
+            float(v['meta']['end_cpu_clock']) - float(v['meta']['start_cpu_clock']),
+            float(v['meta']['end_wall_clock']) - float(v['meta']['start_wall_clock']),
+        ]
+
+    for k,v in benchmarks[95].items():
         priorities['bins'] = v['responsebin']['priority_bins'][1:]
         priorities[k] = v['responsebin']['priority']
 
@@ -134,22 +164,17 @@ def main():
         costs[k] = v['responsebin']['cost']
 
 
-        throughput[k] = [v['throughput_tasks_per_bin']]
+        throughput[k] += [v['throughput_tasks_per_bin']]
         bin_data[k] = [v['bins']['length']['mean'], v['bins']['length']['std']]
 
         response_stat[k] = [v['response_times']['mean'], v['response_times']['std']]
 
-        runtime[k] = [
-            float(v['meta']['end_cpu_clock']) - float(v['meta']['start_cpu_clock']),
-            float(v['meta']['end_wall_clock']) - float(v['meta']['start_wall_clock']),
-        ]
-
-    for k,v in short_benchmarks.items():
-        throughput[k] += [v['throughput_tasks_per_bin']]
         runtime[k] += [
             float(v['meta']['end_cpu_clock']) - float(v['meta']['start_cpu_clock']),
             float(v['meta']['end_wall_clock']) - float(v['meta']['start_wall_clock']),
         ]
+
+
     chart_opts = {
         'tablefmt':"plain",
         'floatfmt':".8f",
@@ -158,6 +183,9 @@ def main():
     }
     print_table('charts/priorities.txt', transpose_data_fmt(priorities, first_col="bins"), chart_opts)
     print_table('charts/costs.txt', transpose_data_fmt(costs, first_col="bins"), chart_opts)
+
+    print_table('charts/simbin.txt', transpose_data_fmt(sim_bin_data, first_col='workload'), chart_opts )
+    print_table('charts/simbinresp.txt', transpose_data_fmt(sim_bin_resp, first_col='workload'), chart_opts )
 
     table_opts = {
         'tablefmt':"latex",
@@ -170,6 +198,7 @@ def main():
     print_table('tables/throughput.tex', transpose_data(transpose_data_fmt(throughput, first_col='')), table_opts , head=throughput_head)
     print_table('tables/bin_stat.tex', transpose_data(transpose_data_fmt(bin_data, first_col='')), table_opts ,head=bin_head)
     print_table('tables/simrun.tex', transpose_data(transpose_data_fmt(runtime, first_col='')), table_opts, head=runtime_head )
+
 
 if __name__ == '__main__':
     main()
